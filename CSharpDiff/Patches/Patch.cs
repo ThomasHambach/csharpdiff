8c9085083cf99574309d5789c8149efd8993f597
Delian Jekov <djekov@datexcorp.com>
2024-01-09 11:54:11 +0200

    feat: new/deleted file mode

--- CSharpDiff/Patches/Patch.cs
+++ CSharpDiff/Patches/Patch.cs
@@ -1,7 +1,7 @@
-using System.Text.RegularExpressions;
 using CSharpDiff.Diffs;
 using CSharpDiff.Diffs.Models;
 using CSharpDiff.Patches.Models;
+using System.Text.RegularExpressions;
 
 namespace CSharpDiff.Patches
 {
@@ -185,13 +185,29 @@ namespace CSharpDiff.Patches
         public string formatPatch(PatchResult diff)
         {
             var ret = new List<string>();
+            var oldFileName = (String.IsNullOrEmpty(diff.OldFileName) ? "/dev/null" : diff.OldFileName);
+            var newFileName = (String.IsNullOrEmpty(diff.NewFileName) ? "/dev/null" : diff.NewFileName);
+
+            ret.Add($"diff --git {oldFileName} {newFileName}");
+
             if (diff.OldFileName == diff.NewFileName)
             {
                 ret.Add("Index: " + diff.OldFileName);
             }
-            ret.Add("===================================================================");
-            ret.Add("--- " + diff.OldFileName + (String.IsNullOrEmpty(diff.OldHeader) ? "" : '\t' + diff.OldHeader));
-            ret.Add("+++ " + diff.NewFileName + (String.IsNullOrEmpty(diff.NewHeader) ? "" : '\t' + diff.NewHeader));
+            else if (diff.OldFileName is null && diff.NewFileName is not null)
+            {
+                ret.Add("new file mode 100644");
+            }
+            else if (diff.NewFileName is null && diff.OldFileName is not null)
+            {
+                ret.Add("deleted file mode 100644");
+            }
+
+
+            ret.Add("--- " + oldFileName + (String.IsNullOrEmpty(diff.OldHeader) ? "" : '\t' + diff.OldHeader));
+            ret.Add("+++ " + newFileName + (String.IsNullOrEmpty(diff.NewHeader) ? "" : '\t' + diff.NewHeader));
+
+
 
             for (var i = 0; i < diff.Hunks.Count(); i++)
             {
